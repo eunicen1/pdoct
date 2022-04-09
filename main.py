ba43e5041098f00b60ef5c5a447d7f23d3dc7640
Eunice Nwaobi <42991373+eunicen1@users.noreply.github.com>
2022-04-08 22:31:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 from pathlib import Path
 import csv
 import cupy as cp
-import time
+from time import time
 from processing import file_tools
 #
 
@@ -34,23 +34,30 @@ reference_frame_index = 10
 padSize = 4096
 
 obj = pdoct.Pdoct()
-import sys
-sys.stdout = open('STDOUT.txt', 'w+')
+
+ref = time()
 dispCoeffs, rawFrame, ref_FFT = obj.process_reference_frame(unp, xml, lut, padSize, dispersion_max_order, dispersion_coefficient_range, depth_start_dispersion, depth_end_dispersion, depth_start_saving, depth_end_saving, calibration_signal_offset, reference_frame_index)
-
-
+ref = time() - ref
+print(dispCoeffs, rawFrame)
 import matplotlib.pyplot as plt
-# plt.imshow(20*np.log10(abs(ref_FFT[:, :].T)), cmap='gray')
-# print(ref_FFT.shape)
-# plt.show()
-# plt.show()
+plt.imshow(20*np.log10(abs(ref_FFT[:, :].T)), cmap='gray')
+ax = plt.gca()
+#hide x-axis
+ax.get_xaxis().set_visible(False)
+#hide y-axis
+ax.get_yaxis().set_visible(False)
+print(ref_FFT.shape)
+plt.show()
 
+vol = time()
 ProcdDataA, ProcdDataB = obj.process_whole_volume(unp, xml, lut, dispCoeffs, padSize, dispersion_max_order, depth_start_saving, depth_end_saving, calibration_signal_offset, reference_frame_index)
+vol = time() - vol
 
+save = time()
 from tifffile import *
-
 imwrite('pdoctA.tif', np.flip(20*np.log10(abs(ProcdDataA))), dtype=np.uint16)
 imwrite('pdoctB.tif', np.flip(20*np.log10(abs(ProcdDataB))), dtype=np.uint16)
-
-
-sys.stdout.close()
+save = time() - save
+print("ref: ", ref)
+print("vol: ", vol)
+print("save_2: ", save)
