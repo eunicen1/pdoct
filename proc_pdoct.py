354a58d6b7327f739ab117f5a519969f669cf2f3
Eunice Nwaobi <42991373+eunicen1@users.noreply.github.com>
2022-04-08 22:31:52 -0700

    Update proc_pdoct.py

--- proc_pdoct.py
+++ proc_pdoct.py
@@ -15,7 +15,6 @@ class Pdoct(object):
 
     # function optimized to run on gpu
     # @jit(target_backend ="cuda")
-    @jit(nopython=True, parallel=True, NUMBA_PARALLEL_DIAGNOSTICS=4)
     def process_reference_frame(self, unp, xml, lut, padSize, dispersion_max_order, dispersion_coefficient_range, depth_start_dispersion, depth_end_dispersion, depth_start_saving, depth_end_saving, calibration_signal_offset, reference_frame_index):
         parameters = file_tools.get_parameters(xml)
 
@@ -62,10 +61,11 @@ class Pdoct(object):
         ref_RawData_DisComp = calculation.compensate_dispersion_phase(ref_RawData_HamWin, kaxis, dispersion_max_order, dispersion_coefficients)
         ref_FFTData_DisComp = cp.fft.fft(ref_RawData_DisComp)
         raw_frame = ref_FFTData_DisComp[:, depth_start_saving:depth_end_saving]
-        return dispersion_coefficients, raw_frame, cp.abs(cp.asnumpy(ref_FFTData_DisComp))
+
+        return dispersion_coefficients, raw_frame, np.abs(cp.asnumpy(ref_FFTData_DisComp))
 
     # function optimized to run on gpu
-    @jit(nopython=True, parallel=True)
+    # @jit(target_backend ="cuda")
     def process_whole_volume(self, unp, xml, lut, dispersion_coefficients, padSize, dispersion_max_order, depth_start_saving, depth_end_saving, calibration_signal_offset, reference_frame_index):
         fid = open(unp, mode='r')
         parameters = file_tools.get_parameters(xml)
@@ -83,12 +83,6 @@ class Pdoct(object):
         file_pathB = Path(Path(unp).parent / 'CplxProcdData' / Path(unp).stem / (Path(unp).stem + 'B.npy'))
         ProcdDataA = file_tools.open_memmap(file_pathA, mode='w+', dtype=np.complex64, shape=(num_bscans, num_ascans, depth_end_saving - depth_start_saving))
         ProcdDataB = file_tools.open_memmap(file_pathB, mode='w+', dtype=np.complex64, shape=(num_bscans, num_ascans, depth_end_saving - depth_start_saving))
-
-        threadsperblock = (8, 8, 8)
-        blockspergrid_x = math.ceil(ProcdDataA.shape[0] / threadsperblock[0])
-        blockspergrid_y = math.ceil(ProcdDataA.shape[1] / threadsperblock[1])
-        blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
-        # increment_a_2D_array[blockspergrid, threadsperblock](ProcdDataA)
 
         for i in range(num_bscans):#range(bscan_size):
             print(i)
@@ -108,7 +102,7 @@ class Pdoct(object):
             ref_RawData_FPNSub_A = ref_RawData_rescaled_A - (cp.tile(calculation.median_cp(cp.real(ref_RawData_rescaled_A), axis=1).reshape(1, 1, num_points//2), [1, num_ascans, 1]) + 1j*cp.tile(calculation.median_cp(cp.imag(ref_RawData_rescaled_A), axis=1).reshape(1, 1, num_points//2), [1, num_ascans, 1]))
 
             ref_RawData_FPNSub_B = ref_RawData_rescaled_B - (cp.tile(calculation.median_cp(cp.real(ref_RawData_rescaled_B), axis=1).reshape(1, 1, num_points//2), [1, num_ascans, 1]) + 1j*cp.tile(calculation.median_cp(cp.imag(ref_RawData_rescaled_B), axis=1).reshape(1, 1, num_points//2), [1, num_ascans, 1]))
-            ref_RawData_FPNSub = cp.concatenate((ref_RawData_FPNSub_A, ref_RawData_FPNSub_B), axis=1)
+            ref_RawData_FPNSub = np.concatenate((ref_RawData_FPNSub_A, ref_RawData_FPNSub_B), axis=1)
             # print("fpn", ref_RawData_FPNSub.shape)
             window = cp.tile(cp.hanning(num_points//2), [1, num_ascans*2, 1])
             ref_RawData_HamWin = cp.multiply(window, ref_RawData_FPNSub)
@@ -137,8 +131,12 @@ class Pdoct(object):
         del ProcdDataB
         gc.collect()
 
+        from time import time
+        save_1 = time()
         ProcdDataA = file_tools.open_memmap(file_pathA, mode='r', dtype=np.complex64, shape=(num_bscans, num_ascans, depth_end_saving-depth_start_saving))
         ProcdDataB = file_tools.open_memmap(file_pathB, mode='r', dtype=np.complex64, shape=(num_bscans, num_ascans, depth_end_saving-depth_start_saving))
         ProcdDataA = calculation.reorder_bscan(ProcdDataA, num_mscans)
         ProcdDataB = calculation.reorder_bscan(ProcdDataB, num_mscans)
+        save_1 = time() - save_1
+        print("second save: ", save_1)
         return ProcdDataA, ProcdDataB
